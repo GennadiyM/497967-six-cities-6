32babadca8268f5570b5a86ba49d69323717d899
Gennadiy <mr.gennadiy.maksimov@gmail.com>
2021-02-26 20:07:14 +0300

    done

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page';
+import LoginPage from '../login-page/login-page';
+import FavoritePage from '../favorites-page/favorites-page';
+import RoomPage from '../room-page/room-page';
+import NotFoundPage from '../not-found-page/not-found-page';
 
 const App = (props) => {
     const {cardsCount} = props;
 
     return (
-        <MainPage 
-            cardsCount = {cardsCount}
-        />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <MainPage 
+                        cardsCount = {cardsCount}
+                    />
+                </Route>
+                <Route exact path="/login">
+                    <LoginPage />
+                </Route>
+                <Route exact path="/favorite">
+                    <FavoritePage />
+                </Route>
+                <Route exact path="/room/:id?">
+                    <RoomPage />
+                </Route>
+                <Route>
+                    <NotFoundPage />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 };
 
---
+++ src/components/favorites-page/favorites-page.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Header from '../header/header';
+import PlaceCard from '../place-card/place-card';
+
+const FavoritesPage = () => {
+    return (
+    <div className="page">
+      <Header />
+      
+      <main className="page__main page__main--favorites">
+        <div className="page__favorites-container container">
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <ul className="favorites__list">
+              <li className="favorites__locations-items">
+                <div className="favorites__locations locations locations--current">
+                  <div className="locations__item">
+                    <a className="locations__item-link" href="#">
+                      <span>Amsterdam</span>
+                    </a>
+                  </div>
+                </div>
+                <div className="favorites__places">
+                  <PlaceCard width="80%" modifier="favorites__card" />
+                  <PlaceCard width="100%" modifier="favorites__card" />
+                </div>
+              </li>
+              <li className="favorites__locations-items">
+                <div className="favorites__locations locations locations--current">
+                  <div className="locations__item">
+                    <a className="locations__item-link" href="#">
+                      <span>Cologne</span>
+                    </a>
+                  </div>
+                </div>
+                <div className="favorites__places">
+                  <PlaceCard width="100%" modifier="favorites__card" />
+                </div>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </main>
+      <footer className="footer container">
+        <a className="footer__logo-link" href="main.html">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
+        </a>
+      </footer>
+    </div>
+    );
+};
+
+export default FavoritesPage;
